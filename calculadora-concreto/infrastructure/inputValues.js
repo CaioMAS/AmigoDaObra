0192aa77e40cdadef2b5a22c73ddbe7e5cb5d4ec
Dr4kk0nnys <renatoversianidrakk@gmail.com>
2023-02-20 10:55:33 -0300

    Added new suggestions / Refactored functions to use arrow functions instead of normal ones / Added new infrastructure folder with enum containing values for the props / Refactored if else logic with new improved array checking logic / Cleared var variables and refactored overall variables

---
+++ calculadora-concreto/infrastructure/inputValues.js
@@ -0,0 +1,14 @@
+/* 
+    Note: Being more used to use typescript than javascript,
+    I would use an enum holding the values. Since javascript
+    doesn't support enums yet, I'm using an object.
+*/
+
+const inputValues = {
+    propCimento: `propCimento`,
+    propAreia: `propAreia`,
+    propBrita: `propBrita`,
+    volumeConcreto: `volumeConcreto`,
+}
+
+export { inputValues }
